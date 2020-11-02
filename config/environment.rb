a79541a4385f752c02d8228402e9b3da484db6ef
Clementine <clementine.fouque@gmail.com>
2020-11-02 20:32:18 +0100

    mails working

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,10 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
-  :domain => 'mon_site78@yopmail.com',
+  :domain => 'yopmail.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
-
 
